a29cb1a190d09a490282ed9ddce97c51901f6d8a
João Nuno Matos <joao.nuno.matos@fe.up.pt>
2019-11-08 23:33:20 +0000

    Updated db/squema.sql
    Implemented the MVP data schema for the database.
    
    Based the schema on
    [this design](https://dbdiagram.io/d/5dc5faceedf08a25543db542).

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,41 @@
-DROP TABLE IF EXISTS Place;
-CREATE TABLE Place (
-    id INTEGER PRIMARY KEY,
-    price_per_day REAL NOT NULL,
-    description VARCHAR NOT NULL,
-    location INT NOT NULL
+DROP TABLE IF EXISTS User;
+CREATE TABLE User (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL,
+  username VARCHAR NOT NULL UNIQUE,
+  full_name VARCHAR,
+  country VARCHAR(2),
+  bio VARCHAR
 );
 
-INSERT INTO Place(price_per_day, description, location) VALUES (20, "Best place on earth!", 351);
-INSERT INTO Place(price_per_day, description, location) VALUES (10, "Make out alive and you'll receive a discount!", 350);
+DROP TABLE IF EXISTS PlaceLocation;
+CREATE TABLE PlaceLocation(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  country VARCHAR(2) NOT NULL,
+  city VARCHAR,
+  address VARCHAR,
+  latitude REAL,
+  longitude REAL
+);
+
+DROP TABLE IF EXISTS Place;
+CREATE TABLE Place (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR NOT NULL,
+  place_owner INTEGER REFERENCES User(id) NOT NULL,
+  price_per_day REAL NOT NULL,
+  description VARCHAR,
+  country VARCHAR(2) NOT NULL,
+  city place_location INT REFERENCES PlaceLocation(id) NOT NULL
+);
+
+DROP TABLE IF EXISTS Rental;
+CREATE TABLE Rental (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  guest INTEGER REFERENCES User(id) NOT NULL,
+  place INTEGER REFERENCES Place(id) NOT NULL,
+  checkin_date TEXT NOT NULL,
+  checkout_date TEXT NOT NULL,
+  UNIQUE(place, checkin_date, checkout_date)
+);
