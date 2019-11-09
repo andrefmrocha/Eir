fcd9338d38df98a1f0c411d30735637aad2304af
Andre Rocha <andrefmrocha@live.com.pt>
2019-11-09 19:49:39 +0000

    Added filters section

--- templates/common.php
+++ templates/common.php
@@ -7,16 +7,16 @@ function drawHeader()
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="/js/houses.js" type="module"></script>
-        <script src="/js/hamburguer.js" type="module"></script>
-        <link href="styles/style.css" rel="stylesheet">
-        <link href="styles/responsive.css" rel="stylesheet">
+        <script src="../js/houses.js" type="module"></script>
+        <script src="../js/hamburguer.js" type="module"></script>
+        <link href="../styles/style.css" rel="stylesheet">
+        <link href="../styles/responsive.css" rel="stylesheet">
         <title>Eir</title>
     </head>
 
     <body>
         <nav>
-            <img src="assets/logo.svg" alt="Eir logo">
+            <img src="../assets/logo.svg" alt="Eir logo">
             <div>☰</div>
             <ul>
                 <li>
@@ -99,51 +99,3 @@ function drawHeader()
 <?php } ?>
 
 
-<?php
-function drawMainPage()
-{
-
-    ?>
-    <section id="mainpage-section">
-        <img id="mainpage-img" src="assets/house.jpg" alt="Beautiful House">
-        <span class="card">
-            <div>
-                Find <h6> the place </h6> to stay that best suits your <h6> personality </h6>
-            </div>
-            <form action="pages/list-houses.php" method="GET">
-                <div>
-                    <label for="country">
-                        Country
-                        <input id="country" type="text">
-                    </label>
-                </div>
-                <div>
-                    <label for="city">
-                        City
-                        <input id="city" type="text">
-                    </label>
-                </div>
-                <div id="dates">
-                    <label for="check-in">
-                        Check In
-                        <input id="check-in" type="date">
-                    </label>
-                    <label for="check-out">
-                        Check Out
-                        <input id="check-out" type="date">
-                    </label>
-                </div>
-                <div>
-                    <label for="people">
-                        People
-                        <input id="people" type="number">
-                    </label>
-                </div>
-                <div>
-                    <input id="submit" type="submit" value="Search">
-                </div>
-            </form>
-        </span>
-    </section>
-
-<?php } ?>
