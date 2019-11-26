da85de6ecaea2ed0c3b6e05a69b9ef0e8e7865ff
Andre Rocha <andrefmrocha@live.com.pt>
2019-11-26 17:49:09 +0000

    Implemented api for login and signup

---
+++ pages/sign_up_page.php
@@ -0,0 +1,54 @@
+<?php
+function drawMainPage()
+{
+
+    ?>
+    <section id="mainpage-section">
+        <img id="mainpage-img" src="../assets/house.jpg" alt="Beautiful House">
+        <span class="card">
+            <div>
+                Find <h6> the place </h6> to stay that best suits your <h6> personality </h6>
+            </div>
+            <form action="list_houses.php" method="get">
+                <div>
+                    <label for="country">
+                        Country
+                        <input name="country" id="country" type="text">
+                    </label>
+                </div>
+                <div>
+                    <label for="city">
+                        City
+                        <input name="city" id="city" type="text">
+                    </label>
+                </div>
+                <div id="dates">
+                    <label for="check-in">
+                        Check In
+                        <input name="checkin" id="check-in" type="date">
+                    </label>
+                    <label for="check-out">
+                        Check Out
+                        <input name="checkout" id="check-out" type="date">
+                    </label>
+                </div>
+                <div>
+                    <label for="people">
+                        People
+                        <input name="people" id="people" type="number">
+                    </label>
+                </div>
+                <div>
+                    <input id="submit" type="submit" value="Search">
+                </div>
+            </form>
+        </span>
+    </section>
+
+<?php }
+
+include_once('../templates/common.php');
+drawHeader(['../js/main_page.js']);
+drawMainPage();
+drawFooter();
+?>
