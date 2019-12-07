9207f570c6e70dc3c1300818a8f1635e118bab3b
Andre Rocha <andrefmrocha@live.com.pt>
2019-12-07 13:44:01 +0000

    Added support for less than 2 rentals

--- js/profile_page.js
+++ js/profile_page.js
@@ -4,7 +4,6 @@ import { getPersonPhoto, getPlacePhoto } from './image.js';
 import { removeError, showError, validateEmail } from './form_validation.js';
 import carousel from './carousel.js';
 
-
 const profilePic = document.querySelector('.card section div img');
 const picInput = document.querySelector('#profile-picture');
 const fullname = document.querySelector('#full-name');
@@ -112,26 +111,33 @@ async function getRentalHistory() {
 
   const housesNode = document.querySelector('#rentals-history > div > p');
 
-  carousel.photos = houses.map(house => {
-    const image = document.createElement('img');
-    image.src = getPlacePhoto(house.photo);
-    const wrapper = document.createElement('span');
-    wrapper.appendChild(image);
-    const title = document.createElement('p');
-    title.innerText = house.title;
-    const dates = document.createElement('p');
-    dates.innerHTML = `Between <strong> ${house.checkin} </strong> and <strong> ${house.checkout} </strong>`;
-    wrapper.appendChild(title);
-    wrapper.appendChild(dates);
-    return wrapper;
-  });
+  carousel.photos = houses.map(buildHouse);
   carousel.buildCarousel(housesNode);
 
   const left = document.querySelector('.fa-chevron-left');
   const right = document.querySelector('.fa-chevron-right');
 
-  left.addEventListener('click', () => carousel.previous(housesNode));
-  right.addEventListener('click', () => carousel.next(housesNode));
+  if (houses.length > 2) {
+    left.addEventListener('click', () => carousel.previous(housesNode));
+    right.addEventListener('click', () => carousel.next(housesNode));
+  } else {
+    left.remove();
+    right.remove();
+  }
+}
+
+function buildHouse(house) {
+  const image = document.createElement('img');
+  image.src = getPlacePhoto(house.photo);
+  const wrapper = document.createElement('span');
+  wrapper.appendChild(image);
+  const title = document.createElement('p');
+  title.innerText = house.title;
+  const dates = document.createElement('p');
+  dates.innerHTML = `Between <strong> ${house.checkin} </strong> and <strong> ${house.checkout} </strong>`;
+  wrapper.appendChild(title);
+  wrapper.appendChild(dates);
+  return wrapper;
 }
 
 getProfile();
