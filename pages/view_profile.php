0e34c643013257faa1d833f4311d1c54d55a80ac
João Nuno Matos <joao.nuno.matos@fe.up.pt>
2019-12-14 16:05:20 +0000

    add page skeleton

---
+++ pages/view_profile.php
@@ -0,0 +1,41 @@
+<?php
+function drawPageBody()
+{
+    ?>
+    <section id="user-details" class="user-form profile">
+        <img id="signup-img" src="../assets/login-house.jpg" alt="Beautiful House">
+        <span class="card profile">
+            <section>
+                <img src="../assets/profile-leaf.svg" alt="Profile ornament left leaf" />
+                <div class="profile-picture">
+                </div>
+                <img src="../assets/profile-leaf.svg" alt="Profile ornament left leaf" />
+            </section>
+            <p id="full-name"></p>
+            <section id="country">
+                <i class="fas fa-flag"></i>
+                <p></p>
+            </section>
+            <section id="bio">
+                <h3>About</h3>
+                <p></p>
+            </section>
+        </span>
+    </section>
+    <section id="rentals-history">
+        <h3>Reviews</h3>
+        <div>
+            <i class="fas fa-chevron-left"></i>
+            <p>
+
+            </p>
+            <i class="fas fa-chevron-right"></i>
+        </div>
+    </section>
+<?php }
+
+include_once('../templates/common.php');
+drawHeader([], []);
+drawPageBody();
+drawFooter();
+?>
